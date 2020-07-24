74ddde0a65d2509f0515f2b962391dc17d884318
Peter Phan <pphan@hashicorp.com>
2020-07-24 13:37:37 -0700

    update ws-hashicat-gcp

--- examples/1-create-workspaces/ws-hashicat-gcp.tf
+++ examples/1-create-workspaces/ws-hashicat-gcp.tf
@@ -1,47 +1,47 @@
-module "hashicat-gcp" {
-  source              = "../../modules/tfe"
-  organization        = var.tfc_org
-  workspace_name      = "hashicat-gcp"
-  auto_apply          = false
-  tf_version          = "0.12.29"
-  # VCS Section - if you don't want VCS then comment out section below.
-  vcs_repo  = [
-    {
-      vcs_repo_identifier = "phanclan/hashicat-gcp"
-      working_directory   = ""
-      workspace_branch    = "" # default: master
-      oauth_token_id      = var.oauth_token_id
-    }
-  ]
-}
+# module "hashicat-gcp" {
+#   source              = "../../modules/tfe"
+#   organization        = var.tfc_org
+#   workspace_name      = "hashicat-gcp"
+#   auto_apply          = false
+#   tf_version          = "0.12.29"
+#   # VCS Section - if you don't want VCS then comment out section below.
+#   vcs_repo  = [
+#     {
+#       vcs_repo_identifier = "phanclan/hashicat-gcp"
+#       working_directory   = ""
+#       workspace_branch    = "" # default: master
+#       oauth_token_id      = var.oauth_token_id
+#     }
+#   ]
+# }
 
-output "ws-hashicat-gcp_ids" {
-  value = module.hashicat-gcp.workspace_id
-}
+# output "ws-hashicat-gcp_ids" {
+#   value = module.hashicat-gcp.workspace_id
+# }
 
-resource "tfe_variable" "prefix" {
-   key = "prefix"
-   value = var.prefix
-   category = "terraform"
-   # Try to Never Reveal this in statefiles our output
-   sensitive = false
-   workspace_id = module.hashicat-gcp.workspace_id
-}
+# resource "tfe_variable" "prefix" {
+#    key = "prefix"
+#    value = var.prefix
+#    category = "terraform"
+#    # Try to Never Reveal this in statefiles our output
+#    sensitive = false
+#    workspace_id = module.hashicat-gcp.workspace_id
+# }
 
-# Pass down the secrets from 1-create-workspace to
-# the job being created.
-resource "tfe_variable" "project" {
-   key = "project"
-   value = var.project
-   category = "terraform"
-   sensitive = false # Never Reveal this in statefiles our output
-   workspace_id = module.hashicat-gcp.workspace_id
-}
+# # Pass down the secrets from 1-create-workspace to
+# # the job being created.
+# resource "tfe_variable" "project" {
+#    key = "project"
+#    value = var.project
+#    category = "terraform"
+#    sensitive = false # Never Reveal this in statefiles our output
+#    workspace_id = module.hashicat-gcp.workspace_id
+# }
 
-resource "tfe_variable" "google_credentials" {
-   key = "GOOGLE_CREDENTIALS"
-   value = var.GOOGLE_CREDENTIALS
-   category = "env"
-   sensitive = false # Never Reveal this in statefiles our output
-   workspace_id = module.hashicat-gcp.workspace_id
-}
+# resource "tfe_variable" "google_credentials" {
+#    key = "GOOGLE_CREDENTIALS"
+#    value = var.GOOGLE_CREDENTIALS
+#    category = "env"
+#    sensitive = false # Never Reveal this in statefiles our output
+#    workspace_id = module.hashicat-gcp.workspace_id
+# }
