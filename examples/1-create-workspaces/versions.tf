4f77d5b8f8311a2beec52f89c073894ffcd2220e
Peter Phan <pphan@hashicorp.com>
2020-07-24 13:55:01 -0700

    update ws-hashicat-gcp

--- examples/1-create-workspaces/versions.tf
+++ examples/1-create-workspaces/versions.tf
@@ -3,5 +3,16 @@ provider "tfe" {
 }
 
 terraform {
-  required_version = ">=v0.12.28"
+  required_version = ">=v0.12.29"
+}
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "pphan"
+
+    workspaces {
+      name = "1-create-workspaces"
+    }
+  }
 }
