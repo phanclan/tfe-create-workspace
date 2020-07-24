62d7509b0b5f7d5ca55af42ab089d1762ef03f35
Peter Phan <pphan@hashicorp.com>
2020-07-24 16:12:58 -0700

    declare AWS vars

--- examples/1-create-workspaces/variables.tf
+++ examples/1-create-workspaces/variables.tf
@@ -5,3 +5,5 @@ variable "oauth_token_id" {}
 variable "prefix" { default = "pphan" }
 variable "project" { default = "pphan-test-app-dev" }
 variable "GOOGLE_CREDENTIALS" {}
+variable "AWS_ACCESS_KEY_ID" { default = "CHANGEME"}
+variable "AWS_SECRET_ACCESS_KEY" { default = "CHANGEME"}
