9b8380bea090e7e7fa83d4f100e9b3d20d3bdc1f
Peter Phan <pphan@hashicorp.com>
2020-07-24 18:11:25 -0700

    add vcs to hashicat-aws

--- examples/1-create-workspaces/ws-hashicat-aws.tf
+++ examples/1-create-workspaces/ws-hashicat-aws.tf
@@ -15,15 +15,15 @@ module "hashicat-aws" {
   workspace_name      = "hashicat-aws"
   auto_apply          = true
   tf_version          = "0.12.29"
-  # VCS Section - if you don't want VCS then comment out section below.
-  # vcs_repo  = [
-  #   {
-  #     vcs_repo_identifier = "phanclan/hashicat-aws"
-  #     working_directory   = ""
-  #     workspace_branch    = "" # default: master
-  #     oauth_token_id      = var.oauth_token_id
-  #   }
-  # ]
+  VCS Section - if you don't want VCS then comment out section below.
+  vcs_repo  = [
+    {
+      vcs_repo_identifier = "phanclan/hashicat-aws"
+      working_directory   = ""
+      workspace_branch    = "" # default: master
+      oauth_token_id      = var.oauth_token_id
+    }
+  ]
 }
 
 output "ws-hashicat-aws_id" {
