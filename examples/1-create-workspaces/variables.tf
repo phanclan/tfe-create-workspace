c89b01c82dbc217a9618da67d0ba511f0e6006d6
Peter Phan <pphan@hashicorp.com>
2020-08-14 10:53:02 -0700

    update

--- examples/1-create-workspaces/variables.tf
+++ examples/1-create-workspaces/variables.tf
@@ -4,10 +4,10 @@ variable "tfc_org" {
 
 variable "workspace_ids" {}
 variable "queue_all_runs" { default = "false" }
-variable "oauth_token_id" {}
+variable "oauth_token_id" { default = "CHANGEME" }
 variable "prefix" { default = "pphan" }
 variable "project" { default = "pphan-test-app-dev" }
-variable "GOOGLE_CREDENTIALS" {}
+variable "GOOGLE_CREDENTIALS" { default = "CHANGEME" }
 variable "AWS_ACCESS_KEY_ID" { default = "CHANGEME" }
 variable "AWS_SECRET_ACCESS_KEY" { default = "CHANGEME" }
 variable "AWS_SESSION_TOKEN" { default = "CHANGEME" }
