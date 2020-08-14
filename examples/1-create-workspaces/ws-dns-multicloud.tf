c72e777f0463a8d784692243f3d5ee9f0ad455c8
Peter Phan <pphan@hashicorp.com>
2020-08-13 21:19:09 -0700

    update

--- examples/1-create-workspaces/ws-dns-multicloud.tf
+++ examples/1-create-workspaces/ws-dns-multicloud.tf
@@ -27,14 +27,14 @@ module "dns-multicloud" {
   ]
 }
 
-data "tfe_workspace_ids" "1-create-workspaces" {
+data "tfe_workspace_ids" "ws-create-workspaces" {
   names        = [1-create-workspaces]
   organization = var.tfc_org
 }
 
 resource "tfe_run_trigger" "dns-multicloud_trigger" {
   workspace_id  = module.dns-multicloud.workspace_id
-  sourceable_id = data.tfe_workspace_ids.1-create-workspaces.external_ids
+  sourceable_id = data.tfe_workspace_ids.ws-create-workspaces.external_ids
 }
 
 output "ws-dns-multicloud_id" {
